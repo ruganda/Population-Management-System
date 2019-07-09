9212c4f0f9561571afca0a98b3f13d0852361ffe
ruganda <mubaruganda@gmail.com>
2019-07-09 16:03:29 +0300

    configure express server

---
+++ server.js
@@ -0,0 +1,26 @@
+
+import express from 'express'
+import chalk from 'chalk';
+import models from './models/index';
+import { bodyParser } from 'json-server';
+import Routes from './routers'
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(bodyParser);
+app.use(Routes);
+
+app.get('/', (req, res)=>{
+  res.send("welcome to the population management system")
+})
+
+app.listen(port, function(err){
+  if(err){
+    console.log(err)
+  }else{
+    console.log(chalk.green(`App running at http://localhost:${port}`))
+  }
+});
+
+export default app;
